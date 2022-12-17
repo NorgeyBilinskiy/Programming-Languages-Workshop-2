bb68d8af7031b5600349d3088a2ce2c278063efe
Liandon <bilinskiy-01@mail.ru>
2022-12-17 12:44:29 +0600

    3

--- task10/Program.cs
+++ task10/Program.cs
@@ -2,10 +2,13 @@
 
 Console.WriteLine("Добрый день!");
 
-Console.WriteLine("Введите 3-ёх значное число");
+Console.WriteLine("Введите трёхзначное значное число");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int a1 = num/10;
-int x2 = a1 % 10;
-
-Console.WriteLine($"Вторая цифра данного числа {x2}");
+if (num<100) Console.WriteLine($"Ошибка! Вы ввели не трёхзначное число!");
+else if (num>999) Console.WriteLine($"Ошибка! Вы ввели не трёхзначное число!");
+else {
+    int a1 = num/10;
+    int x2 = a1 % 10;
+    Console.WriteLine($"Вторая цифра данного числа {x2}");
+    };
